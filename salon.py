6e387fb9425c10a3ce3afc61116aada2aa358b90
Kevin Edmond <135303747+kaedmond24@users.noreply.github.com>
2023-12-21 22:45:11 -0500

    Update salon.py

--- salon.py
+++ salon.py
@@ -1,8 +1,11 @@
-
+# All list items corralates by index
+# List of Hairstyles
 hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
 
+# List of Prices
 prices = [30, 25, 40, 20, 20, 35, 50, 35]
 
+# List of Sales during the last week
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
 # Carly wants to be able to market her low prices.
